a2c7a463ab5dd7826f7e490a0df202bb547f5732
Ramish Amir <ramishamir2750@gmail.com>
2025-04-29 21:12:04 -0400

    Added element adder component

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,3 +1,12 @@
+import ElementAdder from "@/components/ElementAdder";
+
 export default function Create() {
-  return <h2>Create Page</h2>;
+  return (
+    <section className="flex justify-between gap-4 py-20 px-36 w-full min-h-screen">
+      <ElementAdder />
+      <div className="bg-white rounded-md p-4 h-max w-3/4 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+        <h1>Form</h1>
+      </div>
+    </section>
+  );
 }
---
+++ src/components/ElementAdder.tsx
@@ -0,0 +1,111 @@
+export default function ElementAdder() {
+  const controlButtons = [
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <g fill="none">
+            <path
+              fill="currentColor"
+              fillRule="evenodd"
+              d="M7.934 2h8.132c.886 0 1.65 0 2.262.082c.655.088 1.284.287 1.793.797c.51.51.709 1.138.797 1.793C21 5.284 21 6.048 21 6.934V7.95a1 1 0 1 1-2 0V7c0-.971-.002-1.599-.064-2.061c-.059-.434-.153-.57-.229-.646s-.212-.17-.646-.229C17.6 4.002 16.971 4 16 4h-3v17a1 1 0 1 1-2 0V4H8c-.971 0-1.599.002-2.061.064c-.434.059-.57.153-.646.229s-.17.212-.229.646C5.002 5.4 5 6.029 5 7v.95a1 1 0 1 1-2 0V6.934c0-.886 0-1.65.082-2.262c.088-.655.287-1.284.797-1.793c.51-.51 1.138-.709 1.793-.797C6.284 2 7.048 2 7.934 2"
+              clipRule="evenodd"
+            />
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M7 21h10"
+            />
+          </g>
+        </svg>
+      ),
+      label: "Text",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 48 48"
+        >
+          <g
+            fill="none"
+            stroke="currentColor"
+            strokeLinejoin="round"
+            strokeWidth="4"
+          >
+            <path d="M8 6a2 2 0 0 1 2-2h20l10 10v28a2 2 0 0 1-2 2H10a2 2 0 0 1-2-2z" />
+            <path strokeLinecap="round" d="M16 20h16m-16 8h16" />
+          </g>
+        </svg>
+      ),
+      label: "Paragraph",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <g
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+          >
+            <path d="m9 11l3 3l8-8" />
+            <path d="M20 12v6a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h9" />
+          </g>
+        </svg>
+      ),
+      label: "Checkbox",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 256 256"
+        >
+          <path
+            fill="currentColor"
+            d="M80 64a8 8 0 0 1 8-8h128a8 8 0 0 1 0 16H88a8 8 0 0 1-8-8m136 56H88a8 8 0 0 0 0 16h128a8 8 0 0 0 0-16m0 64H88a8 8 0 0 0 0 16h128a8 8 0 0 0 0-16M44 52a12 12 0 1 0 12 12a12 12 0 0 0-12-12m0 64a12 12 0 1 0 12 12a12 12 0 0 0-12-12m0 64a12 12 0 1 0 12 12a12 12 0 0 0-12-12"
+          />
+        </svg>
+      ),
+      label: "Select",
+    },
+  ];
+
+  return (
+    <div className="bg-white rounded-md p-4 h-max w-1/4 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+      <div>
+        <h2 className="font-semibold">Add Form Elements</h2>
+      </div>
+      <div className="flex flex-wrap gap-2.5 mt-4">
+        {controlButtons?.map((button, index) => {
+          return (
+            <button
+              key={index}
+              className="flex flex-col cursor-pointer transition duration-300 ease-in-out hover:bg-gray-100 w-[calc(50%-5px)] items-center justify-center gap-2 p-4 rounded-md shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
+            >
+              {button.icon}
+              <span>{button.label}</span>
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
